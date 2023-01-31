05714b1bb45d6683bcef54bb7a5b7b396fd3a7b6
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-01-31 20:03:45 +0200

    handle error

--- non-blocking.js
+++ non-blocking.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 
 // callback to achieve asynchronous => callback hells
 fs.readFile('./data/start.txt', 'utf-8', (error, data1) => {
+    if (error) {
+        // show message to user
+        // log to a server
+        // send a notification for dev teams
+        return console.log('ERROR!');
+    }
     fs.readFile(`./data/${data1}.txt`, 'utf-8', (error, data2) => {
         fs.readFile(`./data/append.txt`, 'utf-8', (error, data3) => {
             fs.writeFile('./data/final.txt', `${data2}\n${data3}`, 'utf-8', error => {
